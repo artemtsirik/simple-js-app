3dd19633069cb7dd8f4d13b838b11b311d27c679
artemtsirik <artemtsirik@gmail.com>
2023-09-04 20:20:35 -0700

    This repository includes a list of pokemons with: name, height & type;

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert("Hello world");
-let MyFood = "Meat";
-document.write(MyFood);
+let pokemonList = [
+  { name: 'Bulbasaur', height: '0.7 m', type: ['grass', 'poison'] },
+  { name: 'Ivysaur', height: '1 m', type: ['grass', 'poison'] },
+  { name: 'Pikachu', height: '0.4 m', type: ['electric'] },
+  { name: 'Pidgey', height: '0.3 m', type: ['flying', 'normal'] },
+];
